5e67e7bce383326ccfc26b44b990f553aad79e3b
aditya <1032212829@mitwpu.edu.in>
2025-03-25 17:30:48 +0530

    test

---
+++ index.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".btn")
+btn.addEventListener("click",(e)=>{
+    document.querySelectorAll("select[name='Register']");
+    register.innerHTML="";
+})  
+
+let button=document.querySelector('.btn');
+
+button.addEventListener('click',(e) =>{
+    register1.innerHTML("");
+});
+
