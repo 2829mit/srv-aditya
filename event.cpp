6db82ab7b47443d74d76d226a0ec8f8e6e6d35b4
aditya <1032212829@mitwpu.edu.in>
2025-03-25 17:34:57 +0530

    Initial commit

---
+++ event.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class event{
+    public:
+    void Register();
+    void Browse();
+    void Cancel();
+
+
+};
+void Register(){
+    string name;
+    int id;
+    cout<<"Enter the "
+}
+int main()
+{
+   return 0;
+}
